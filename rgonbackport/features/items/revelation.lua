1d7046bd89548abab29b52f29ad96c56c3fa90a7
catinsurance <65452914+catinsurance@users.noreply.github.com>
2025-11-03 07:34:03 -0800

    Improve Revelation and Seraphim backports

--- rgonbackport/features/items/revelation.lua
+++ rgonbackport/features/items/revelation.lua
@@ -1,10 +1,4 @@
 local mod = RgonBackport
 
-local Constants = {
-    SOUL_HEARTS = 4
-}
-
-function mod:RevelationCollect(_, _, _, _, _, player)
-    player:AddSoulHearts(-Constants.SOUL_HEARTS)
-end
-mod:AddCallback(ModCallbacks.MC_POST_ADD_COLLECTIBLE, mod.RevelationCollect, CollectibleType.COLLECTIBLE_REVELATION)
+local revelation = mod.ItemConfig:GetCollectible(CollectibleType.COLLECTIBLE_REVELATION)
+revelation.AddSoulHearts = 0
--- rgonbackport/features/items/seraphim.lua
+++ rgonbackport/features/items/seraphim.lua
@@ -1,21 +1,4 @@
 local mod = RgonBackport
 
-function mod:SeraphimCollect(_, _, _, _, _, player)
-    player:IncrementPlayerFormCounter(PlayerForm.PLAYERFORM_ANGEL, 1)
-end
-mod:AddCallback(ModCallbacks.MC_POST_ADD_COLLECTIBLE, mod.SeraphimCollect, CollectibleType.COLLECTIBLE_SERAPHIM)
-
-function mod:SeraphimRemove(player)
-    player:IncrementPlayerFormCounter(PlayerForm.PLAYERFORM_ANGEL, -1)
-end
-mod:AddCallback(ModCallbacks.MC_POST_TRIGGER_COLLECTIBLE_REMOVED, mod.SeraphimRemove, CollectibleType.COLLECTIBLE_SERAPHIM)
-
---[[
--- Not needed
-function mod:SeraphimModsLoaded()
-    if EID then
-       EID.CustomTransformAssignments["5.100." .. CollectibleType.COLLECTIBLE_SERAPHIM] = tostring(PlayerForm.PLAYERFORM_ANGEL)
-    end
-end
-mod:AddCallback(ModCallbacks.MC_POST_MODS_LOADED, mod.SeraphimModsLoaded)
-]]
+local seraphim = mod.ItemConfig:GetCollectible(CollectibleType.COLLECTIBLE_SERAPHIM)
+seraphim.Tags = seraphim.Tags | ItemConfig.TAG_ANGEL
